8539ae36946c7b3467ecd34105cde24df1176afb
Watirboi <kwhit@hotmail.com>
2016-08-02 20:17:09 -0500

    clone of tumblr app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
     resources :comments
@@ -8,4 +9,7 @@ Rails.application.routes.draw do
 
   # assign route to About page
   get '/about', to: 'pages#about'
+
+  # Step 2 of devise install
+  root to: "home#index"
 end
